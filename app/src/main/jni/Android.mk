4cf1742c9bbb99bb9c262ae310fec749bb7d7c0b
nihuini <nihuini@tencent.com>
2020-06-16 12:56:19 +0800

    build with ncnn 20200616 and ndk-r21d

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -9,13 +9,33 @@ LOCAL_SRC_FILES := $(NCNN_INSTALL_PATH)/$(TARGET_ARCH_ABI)/libncnn.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := glslang
+LOCAL_SRC_FILES := $(NCNN_INSTALL_PATH)/$(TARGET_ARCH_ABI)/libglslang.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := SPIRV
+LOCAL_SRC_FILES := $(NCNN_INSTALL_PATH)/$(TARGET_ARCH_ABI)/libSPIRV.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := OGLCompiler
+LOCAL_SRC_FILES := $(NCNN_INSTALL_PATH)/$(TARGET_ARCH_ABI)/libOGLCompiler.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := OSDependent
+LOCAL_SRC_FILES := $(NCNN_INSTALL_PATH)/$(TARGET_ARCH_ABI)/libOSDependent.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
 
 LOCAL_MODULE := squeezencnn
 LOCAL_SRC_FILES := squeezencnn_jni.cpp
 
 LOCAL_C_INCLUDES := $(NCNN_INSTALL_PATH)/include $(NCNN_INSTALL_PATH)/include/ncnn
 
-LOCAL_STATIC_LIBRARIES := ncnn
+LOCAL_STATIC_LIBRARIES := ncnn glslang SPIRV OGLCompiler OSDependent
 
 LOCAL_CFLAGS := -O2 -fvisibility=hidden -fomit-frame-pointer -fstrict-aliasing -ffunction-sections -fdata-sections -ffast-math
 LOCAL_CPPFLAGS := -O2 -fvisibility=hidden -fvisibility-inlines-hidden -fomit-frame-pointer -fstrict-aliasing -ffunction-sections -fdata-sections -ffast-math
@@ -23,7 +43,7 @@ LOCAL_LDFLAGS += -Wl,--gc-sections
 
 LOCAL_CFLAGS += -fopenmp
 LOCAL_CPPFLAGS += -fopenmp
-LOCAL_LDFLAGS += -fopenmp
+LOCAL_LDFLAGS += -static-openmp -fopenmp
 
 LOCAL_LDLIBS := -lz -llog -ljnigraphics -lvulkan -landroid
 
